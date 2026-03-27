17281482f25b1f53bde0358659dd331c35e1f0cb
Anna Karutina <anna.karutina@khk.ee>
2026-03-27 12:56:16 +0200

    Lab 1.5 - Research "hea" versioon

--- 1. SOLID/dip/relationships.py
+++ 1. SOLID/dip/relationships.py
@@ -1,9 +1,17 @@
-from realtionship import Relationship
+from relationship import Relationship
+from browser import RelationshipBrowser
 
-class Relationships:
+class Relationships(RelationshipBrowser):
     def __init__(self):
         self.relations = []
 
     def add_parent_and_child(self, parent, child):
         self.relations.append((parent, Relationship.PARENT, child))
         self.relations.append((child, Relationship.CHILD, parent))
+
+    def find_all_children_of(self, name):
+        children = []
+        for relation in self.relations:
+            if relation[0].name == name and relation[1] == Relationship.PARENT:
+                children.append(relation[2].name)
+        return children
--- 1. SOLID/dip/research.py
+++ 1. SOLID/dip/research.py
@@ -1,7 +1,7 @@
-from realtionship import Relationship
-
 class Research:
-    def __init__(self, relationships):
-        for relation in relationships.relations:
-            if relation[0].name == "John" and relation[1] == Relationship.PARENT:
-                print(f"John has a child called {relation[2].name}")
+    def __init__(self, browser):
+        for child in browser.find_all_children_of("John"):
+            print(f"John has a child called {child}")
+
+
+
